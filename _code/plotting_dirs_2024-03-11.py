10d03d6caa35dfebad7193f6b1991a4f11de3287
bruzen <k.w.robinson@gmail.com>
2024-03-11 00:56:34 -0400

    Continue fast edit

--- _code/plotting_dirs_2024-03-11.py
+++ _code/plotting_dirs_2024-03-11.py
@@ -53,7 +53,7 @@ def small_multiples_lineplot(df, param_mapping, palette=None):
     # Show the plot
     plt.show()
 
-def variables_vs_time_extended(df, variable_parameters = None):
+def variables_vs_time(df, variable_parameters = None):
     set_style()
     plt.rcParams['font.size'] = 10
     
@@ -131,165 +131,11 @@ def variables_vs_time_extended(df, variable_parameters = None):
 
         # Plot N
         # axes[1, 1].plot(subset_df['time_step'], subset_df['agglomeration_population'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[1, 1].plot(subset_df['time_step'], subset_df['worker_demand'], label=label, color='red', alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[1, 1].plot(subset_df['time_step'], subset_df['worker_supply'], label=label, color='green', alpha=alpha, linestyle = linestyle, linewidth=linewidth)     
-        #axes[1, 1].plot(subset_df['time_step'], subset_df['worker_supply}'], label=label, color=red, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
+        #axes[1, 1].plot(subset_df['time_step'], subset_df['worker_demand'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
+        # axes[1, 1].plot(subset_df['time_step'], subset_df['worker Demand'], label=label, color='green', alpha=alpha, linestyle = linestyle, linewidth=linewidth)     
+        axes[1, 1].plot(subset_df['time_step'], subset_df['worker_supply'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
         # axes[1, 0].set_xlabel('Time Step')
-        axes[1, 1].set_ylabel('NDred NSGr')
-        # axes[1, 0].set_title(f'Total workforce') # (N)')
-        axes[1, 1].grid(True)
-        axes[1, 1].legend().set_visible(False)
-        axes[1, 1].yaxis.set_major_locator(MaxNLocator(nbins=nbins))
-
-        # Plot city extent
-        axes[2, 0].plot(subset_df['time_step'], subset_df['city_extent_calc'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        # axes[2, 0].set_xlabel('Time Step')
-        axes[2, 0].set_ylabel('City extent \n (lot lengths)')
-        # axes[2, 0].set_title(f'City extent')
-        axes[2, 0].grid(True)
-        axes[2, 0].legend().set_visible(False)
-        axes[2, 0].yaxis.set_major_locator(MaxNLocator(nbins=nbins))
-
-        # Plot F
-        axes[2, 1].plot(subset_df['time_step'], subset_df['F'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        # axes[1, 1].set_xlabel('Time Step')
-        axes[2, 1].set_ylabel('F')
-        # axes[1, 1].set_title(f'Number of firms') # (F)')
-        axes[2, 1].grid(True)
-        axes[2, 1].yaxis.set_major_locator(MaxNLocator(nbins=nbins))
-
-        if 'investor_ownership_share' in subset_df:
-            # Plot 'Owner-occupier_share'
-            axes[3, 0].plot(subset_df['time_step'], (1- subset_df['investor_ownership_share']), label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-            axes[3, 0].set_xlabel('Time Step')
-            axes[3,0].set_ylim(-0.01, 1.01)
-            axes[3, 0].set_ylabel('Owner-occupier \n share') #('Ownership share')
-            # axes[3, 0].set_title('Owner-occupier') #('Owner-occupier fraction')
-            axes[3, 0].grid(True)
-            axes[3, 0].yaxis.set_major_locator(MaxNLocator(nbins=nbins))
-            # Display a single legend outside the figure
-            if(subset_df['iteration'].iloc[0] == 0):
-                axes[3, 0].legend(loc='center left', bbox_to_anchor=(1.2, 0.5), frameon=False)
-        else:
-            axes[2, 0].set_xlabel('Time Step')
-            axes[2, 1].set_xlabel('Time Step')
-            # Display a single legend outside the figure
-            if(subset_df['iteration'].iloc[0] == 0):
-                axes[2, 1].legend(loc='center left', bbox_to_anchor=(-1.3, -1.5), frameon=False)
-            axes[3, 0].set_axis_off()
-        axes[3, 1].set_axis_off()
-
-    # Override font sizes
-    default_font_size = plt.rcParams['font.size']
-    for ax in axes.flatten():
-        ax.set_title(ax.get_title(),   fontsize=default_font_size)
-        ax.set_xlabel(ax.get_xlabel(), fontsize=default_font_size)
-        ax.set_ylabel(ax.get_ylabel(), fontsize=default_font_size)
-
-    name = f'{"-".join(variable_parameters.keys())}-{model_name}-{timestamp}'
-    figure_filepath = file_utils.get_figures_filepath(f'{name}.pdf')
-    label_text = (
-        # name
-        f'{model_name}: {model_description} {timestamp} \n'
-        f'Commit ID {file_utils.get_git_commit_hash()} \n'
-        f'{file_utils.get_git_details()}'
-        # f'\n {name} {" ".join(variable_parameters.keys())}'
-        # f'{figure_filepath}\n'
-        # f'adjF: {model_parameters["adjF"]}, adjw: {model_parameters["adjw"]}, '
-        # f'discount_rate: {model_parameters["discount_rate"]}, r_margin: {model_parameters["r_margin"]},\n'
-        # f'max_mortgage_share: {model_parameters["max_mortgage_share"]}, '
-        # f'cg_tax_per: {model_parameters["cg_tax_per"]}, '
-        # f'cg_tax_invest: {model_parameters["cg_tax_invest"]}'
-    )
-
-    plt.text(-2.1, -1.3, label_text, transform=plt.gca().transAxes, ha='left', va='center', wrap=True)
-    plt.savefig(figure_filepath, format='pdf', bbox_inches='tight')
-
-
-def variables_vs_time(df, variable_parameters = None):
-    set_style()
-    plt.rcParams['font.size'] = 10
-    
-    # df = pd.DataFrame(results)
-    timestamp         = df['timestamp'].iloc[0] # Same timestep for all rows in df
-    model_name        = df['model_name'].iloc[0]
-    model_description = df['model_description'].iloc[0]
-    figures_folder    = file_utils.get_figures_subfolder()
-
-    # TODO move to style
-     # Define plotting styles for runs
-    cmap       = plt.get_cmap('tab10')
-    num_runs   = len(df['RunId'].unique())
-    colors     = [(0.2, 0.6, 1.0), 'black', (0.5, 0.5, 1.0), 'cyan', 'red', 'pink'] # [cmap(i) for i in np.linspace(0, 1, num_runs)]
-    linewidths = [1, .5, 1, 1] # linewidths = [1, 2, 3, 4]
-    linestyles = ['dashed', 'solid', 'dotted', 'dashdot']  # Add more if needed
-    alpha      = 0.8
-    nbins      = 4 # Number of tick marks/grid lines
-    
-    # Create subplots with a 4x2 grid
-    fig, axes = plt.subplots(4, 2, figsize=(.5*PAGE_WIDTH, .7*PAGE_WIDTH), gridspec_kw={'hspace': .78, 'wspace': 0.9})  # 4 rows, 2 columns
-    if 'iteration' in df.columns:
-        df['RunGroup'] = df.groupby('iteration')['RunId'].transform(lambda x: pd.factorize(x)[0])
-
-    # Loop through each run
-    for i, run_id in enumerate(df['RunId'].unique()):
-        # Subset the DataFrame for the current run and exclude time_step 0
-        subset_df = df[(df['RunId'] == run_id)] # & (df['Step'] > 0)]
-
-        if variable_parameters:
-            # Extract variable parameter values for the current RunId
-            variable_values = {param: subset_df[param].iloc[0] for param in variable_parameters.keys()}
-
-            # Construct label using variable parameter values
-            label = f'{", ".join(f"{key} {value}" for key, value in variable_values.items())}'
-        else:
-            label = None
-
-        if 'iteration' in df.columns:
-            run_group = subset_df['RunGroup'].iloc[0]
-        else:
-            run_group = subset_df['RunId'].iloc[0]
-        # Use the defined styles for each run
-        color     =     colors[run_group % len(colors)] # colors[i % len(colors)]
-        linestyle = linestyles[run_group % len(linestyles)]  # linestyles[i % len(linestyles)]  # Cycle through linestyles
-        linewidth = linewidths[run_group % len(linewidths)]  # linewidths[i % len(linewidths)]  # Cycle through linewidths
-
-        # Plot MPL
-        # axes[0, 0].plot(subset_df['time_step'], subset_df['MPL'], label=label, color='red', alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[0, 0].plot(subset_df['time_step'], subset_df['wage'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        # axes[0, 0].set_xlabel('Time Step')
-        axes[0, 0].set_ylabel('Wage ($)')
-        # axes[0, 0].set_title(f'MPL')
-        axes[0, 0].grid(True)
-        axes[0, 0].legend().set_visible(False)
-        axes[0, 0].yaxis.set_major_locator(MaxNLocator(nbins=nbins))
-
-        # Plot 'k'
-        axes[0, 1].plot(subset_df['time_step'], subset_df['k'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        # axes[0, 1].set_xlabel('Time Step')
-        axes[0, 1].set_ylabel('k ($)')
-        # axes[0, 1].set_title(f'Firm capital') # (k)')
-        axes[0, 1].grid(True)
-        axes[0, 1].legend().set_visible(False)
-        axes[0, 1].yaxis.set_major_locator(MaxNLocator(nbins=nbins))
-
-        # Plot n
-        axes[1, 0].plot(subset_df['time_step'], subset_df['n'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        # axes[0, 1].set_xlabel('Time Step')
-        axes[1, 0].set_ylabel('n')
-        # axes[0, 1].set_title(f'Firm workforce') # (n)')
-        axes[1, 0].grid(True)
-        axes[1, 0].legend().set_visible(False)
-        axes[1, 0].yaxis.set_major_locator(MaxNLocator(nbins=nbins))
-
-        # Plot N
-        # axes[1, 1].plot(subset_df['time_step'], subset_df['agglomeration_population'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        # axes[1, 1].plot(subset_df['time_step'], subset_df['worker_demand'], label=label, color='red', alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[1, 1].plot(subset_df['time_step'], subset_df['worker_supply'], label=label, color='green', alpha=alpha, linestyle = linestyle, linewidth=linewidth)     
-        #axes[1, 1].plot(subset_df['time_step'], subset_df['worker_supply}'], label=label, color=red, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        # axes[1, 0].set_xlabel('Time Step')
-        # axes[1, 1].set_ylabel('NDred NSGr')
-        axes[1, 1].set_ylabel('N')
+        axes[1, 1].set_ylabel('Population')
         # axes[1, 0].set_title(f'Total workforce') # (N)')
         axes[1, 1].grid(True)
         axes[1, 1].legend().set_visible(False)
